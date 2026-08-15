6447beb8832e34b4847b07ee8980c1751661b702
Hüseyin Sekmenoğlu <huseyin@sekmen.dev>
2026-08-15 10:40:16 +0300

    fixed lint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,31 @@
-import { defineConfig, globalIgnores } from "eslint/config";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginAstro from 'eslint-plugin-astro';
 
-const eslintConfig = defineConfig([
-  globalIgnores([
-    ".astro/**",
-    "dist/**",
-    "build/**",
-    "html/**",
-    "node_modules/**",
-    "coverage/**",
-    "test-results/**",
-    "playwright-report/**",
-    "vitest-report/**",
-    ".yarn/**",
-    ".pnp.cjs",
-    ".yarnrc.yml"
-  ]),
+export default tseslint.config(
+  {
+    ignores: [
+      ".astro/**",
+      "dist/**",
+      "build/**",
+      "html/**",
+      "node_modules/**",
+      "coverage/**",
+      "test-results/**",
+      "playwright-report/**",
+      "vitest-report/**",
+      ".yarn/**",
+      ".pnp.cjs",
+      ".yarnrc.yml"
+    ]
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "off"
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }]
     }
   }
-]);
-
-export default eslintConfig;
+);
